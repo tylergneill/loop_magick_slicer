445a47a5f75328ad682d3d12e1576f0d85fe9a5a
Tyler Neill <tyler.g.neill@gmail.com>
2020-06-22 10:40:08 -0400

    remove file extension

--- loop_magick_slicer.py
+++ loop_magick_slicer.py
@@ -3,13 +3,15 @@ output_path = 'output'
 input_path = 'input'
 
 # INSTRUCTIONS: 
-# 1) Place all input image files in the input folder.
+# 1) Place all input jpg files in the input folder.
 # 2) Run this Python-2.7 script from the command-line.
 # 3) Wait for the script to finish (up to several seconds per individual file).
 # 4) Look in the output folder for the result.
 
 import os
 import subprocess
+
+def rm_extension(fn, extension): return fn[:fn.find(extension)]
 
 filenames = os.listdir(input_path)
 filenames.sort()
@@ -21,7 +23,7 @@ for fn in filenames:
 
 	run_arg = os.path.join(source_path, 'magick-slicer.sh')
 	input_arg = os.path.join(input_path, fn)
-	output_arg = os.path.join(output_path, fn)
+	output_arg = os.path.join(output_path, rm_extension(fn, '.jpg'))
 	command = "%s %s %s" % (run_arg, input_arg, output_arg)
 	print "command: ", command
 	subprocess.call(command, shell='True')
